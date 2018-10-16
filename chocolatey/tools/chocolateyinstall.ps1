d4e96bd7df72f8146b7bdaea560ed12bcde97bc1
Jason Ginchereau <jasongin@microsoft.com>
2018-10-16 09:02:49 -0700

    Update chocolatey package to 1.5.0 (#108)

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/jasongin/nvs/releases/download/v1.4.2/nvs-1.4.2.msi' # download url, HTTPS preferred
+$url        = 'https://github.com/jasongin/nvs/releases/download/v1.5.0/nvs-1.5.0.msi' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'NVS' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = '87fa567001baea5604e4be03188d6d11ae2a2e055781e5168da5ea2f0f8fb5479aa0c3511b641f5147ef7bbffe03cbff1a36588c1f37a8dd2c27010e24d0d3f9'
+  checksum      = 'f6e753ec426ee41f645334465ab57e61e6e1ab379e9443ec2db10f627da4329dfe3a269c15a6f41b51abfba5be71655c75acc9b3798052c41d99899a0dc01079'
   checksumType  = 'sha512' #default is md5, can also be sha1, sha256 or sha512
 
   # MSI
