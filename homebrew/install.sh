8ff3dcbb265a9ba0464993155d3ac4598a4c85ef
Michael Mao <michael.mao99@gmail.com>
2022-04-12 00:16:19 -0700

    take script location into account when generating shell profile

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,12 +6,5 @@ NVS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 export HOME=~
 export NVS_HOME=~/.nvs
 
-# Link necessary files
-ln -fs $NVS_DIR/nvs.sh $NVS_HOME/nvs.sh
-ln -fs $NVS_DIR/lib $NVS_HOME/lib
-ln -fs $NVS_DIR/deps $NVS_HOME/deps
-ln -fs $NVS_DIR/package.json $NVS_HOME/package.json
-ln -fs $NVS_DIR/defaults.json $NVS_HOME/defaults.json
-
 # Run install script
 \. $NVS_DIR/nvs.sh install
