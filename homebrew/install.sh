6c5a5983e2bccae5ac2b217b69210ecf3cd560f1
Michael Mao <michael.mao99@gmail.com>
2023-01-31 17:17:11 -0800

    add linking to specified location in homebrew install script (#237)

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+LINK_DIR=$1
 NVS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
 # Set environment variables
@@ -8,3 +9,6 @@ export NVS_HOME=~/.nvs
 
 # Run install script
 \. $NVS_DIR/nvs.sh install
+
+# Link to a specified location to avoid changing shell profile while upgrading
+ln -Ffs $NVS_DIR $LINK_DIR
